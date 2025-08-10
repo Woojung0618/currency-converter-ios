5629fafeaaf96d4f22a46ff823c31904550a8976
전우정 <woojung0618@gmail.com>
2025-08-10 14:18:06 +0900

    Initial Commit

---
+++ currency-converter-app/currency_converter_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  currency_converter_appApp.swift
+//  currency-converter-app
+//
+//  Created by 전우정 on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct currency_converter_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ currency-converter-appTests/currency_converter_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  currency_converter_appTests.swift
+//  currency-converter-appTests
+//
+//  Created by 전우정 on 8/10/25.
+//
+
+import Testing
+@testable import currency_converter_app
+
+struct currency_converter_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
